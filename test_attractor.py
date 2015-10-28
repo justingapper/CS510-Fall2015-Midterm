7869d0a48f86f8eb989df3b471ef4ef37b08c3d2
justingapper <justin.gapper@gmail.com>
2015-10-28 18:22:31 +0000

    update attr nosetest

--- test_attractor.py
+++ test_attractor.py
@@ -2,6 +2,7 @@ from attractor import Attractor
 from random import uniform, randint
 from math import sqrt
 from nose import with_setup
+from random import randint
 
 ###
 # Test Suite for specified attractor interface
@@ -12,39 +13,40 @@ from nose import with_setup
 #import unittest
 
 class attr_test:
-#   """Test setup and default/overide initial parameters"""
-    def test_setup(self):
-        attr = Attractor()
-        assert attr.params[0] == 10
-        assert attr.params[1] == 28
-        assert attr.params[2] == 8.0/3.0
+    """Test setup and default/overide initial parameters"""
+    def setup(self):
+        s = randint(1,10)
+        p = randint(1,10)
+        b = randint(1,10)
+        attr = Attractor(s, p, b)
+        attr.s_t = s
+        attr.p_t = p
+        attr.b_t = b
+        attr = Attractor(attr.s_t, attr.p_t, attr.b_t)
+        assert attr.params[0] == s
+        assert attr.params[1] == p
+        assert attr.params[2] == b
         assert attr.start == 0
         assert attr.end == 80
         assert attr.points == 10000
-    
-    """Test euler method outputs results"""
-    
+
     def test_euler(self):
         attr = Attractor()
         attr.evolve([10,10,10],1)
         assert attr.solution['x'].count() > 0
-        #assert attr.solution['y'].count() > 0
-        #assert attr.solution['z'].count() > 0
+        assert attr.solution['y'].count() > 0
+        assert attr.solution['z'].count() > 0
 
-    """Test RK2 method outputs results"""
-    
-    def test_euler(self):
+    def test_rk2(self):
         attr = Attractor()
         attr.evolve([10,10,10],2)
         assert attr.solution['x'].count() > 0
-        #assert attr.solution['y'].count() > 0
-        #assert attr.solution['z'].count() > 0
-        
-        """Test RK4 method outputs results"""
-        
-    def test_euler(self):
+        assert attr.solution['y'].count() > 0
+        assert attr.solution['z'].count() > 0
+
+    def test_rk4(self):
         attr = Attractor()
         attr.evolve([10,10,10],4)
         assert attr.solution['x'].count() > 0
-        #assert attr.solution['y'].count() > 0
-        #assert attr.solution['z'].count() > 0
+        assert attr.solution['y'].count() > 0
+        assert attr.solution['z'].count() > 0
