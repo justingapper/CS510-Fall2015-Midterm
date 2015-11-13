bee9885611053919d3b189571afc1d2b05a37185
justingapper <justin.gapper@gmail.com>
2015-11-13 00:17:51 +0000

    update attractor, test, and readme

--- attractor.py
+++ attractor.py
@@ -1,16 +1,17 @@
-#Indicate version number
-version = 2.0
-#Import numpy, pandas, and matplotlib
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-#import time as dt
 
 class Attractor(object):
-    """Define class and initial parameters s, p, b, start time, end time, and time interval (points).  Calculate dt and create zero arrays to hold values for x, y, z, and t."""
+    """The Attractor class calculates the Euler, 2nd Order Runge-Kutta, 3rd Order Runge-Kutta, and 4th Order Runge-Kutta integration method for a Lorenz Attractor System."""
     def __init__(self, s=10.0, p=28.0, b=8.0/3.0, start=0.0, end=80.0, points=10000):
+        """Define initial conditions:
+        s = Prandtl Number, the ratio of momentum diffusivity and thermal diffusivity. (Default value = 10.0)
+        p = Rayleigh Number to determine whether the heat transfer is primarily in the form of conduction or convection. (Default value = 28.0)
+        b = geometric factor (Default value = 8.0/3.0)
+        start, end, and points are initial, final and time increment variables, respectively. (Default values = 0.0, 80.0, and 10,000)"""
         self.s = s
         self.p = p
         self.b = b
@@ -23,11 +24,11 @@ class Attractor(object):
         self.y = np.zeros([])
         self.z = np.zeros([])
         self.t = np.zeros([])
-
+        
     def fxn1(self, yt, xt, ss, ddt):
         """Define function 1 for dx(t)/dt = s[y(t) - x(t)]"""
         return ss*(yt - xt)*ddt
-
+    
     def fxn2(self, yt, xt, zt, pp, ddt):
         """Define function 2 for dy(t)/dt = x(t)[p - z(t)] - y(t)"""
         return (xt*(pp - zt) - yt)*ddt
@@ -66,16 +67,16 @@ class Attractor(object):
         self.x[0] = xyzarray[0]
         self.y[0] = xyzarray[1]
         self.z[0] = xyzarray[2]
-
+        
         for i in xrange(self.points):
             k1_x = self.fxn1(self.y[i],self.x[i],self.s,self.dt)
             k2_x = self.fxn1(self.y[i],k1_x*(.5*self.dt)+self.x[i],self.s,self.dt)
             self.x[i+1] = self.x[i] + (.5*(k1_x + k2_x))
-
+            
             k1_y = self.fxn2(self.y[i],self.x[i],self.z[i],self.p,self.dt)
             k2_y = self.fxn2(self.y[i]+k1_y*(.5*self.dt),self.x[i],self.z[i],self.p,self.dt)
             self.y[i+1] = self.y[i] + (.5*(k1_y + k2_y))
-
+            
             k1_z = self.fxn3(self.y[i],self.x[i],self.z[i],self.b,self.dt)
             k2_z = self.fxn3(self.y[i],self.x[i],self.z[i]+k1_z*(.5*self.dt),self.b,self.dt)
             self.z[i+1] = self.z[i] + (.5*(k1_z + k2_z))
@@ -84,7 +85,6 @@ class Attractor(object):
                
         return (self.x,self.y,self.z)
     
-
     def rk4(self, xyzarray):
         """Define 4th order Runge-Kutta method.  Create numpy array for results and iterate through the differential equations fxn1, fxn2, and fxn3.
         k3 = y(t+dt/2, x(t) + k1dt/2)
@@ -97,7 +97,7 @@ class Attractor(object):
         self.x[0] = xyzarray[0]
         self.y[0] = xyzarray[1]
         self.z[0] = xyzarray[2]
-
+       
         for i in xrange(self.points):
             k1_x = self.fxn1(self.y[i],self.x[i],self.s,self.dt)
             k2_x = self.fxn1(self.y[i],k1_x*(.5*self.dt)+self.x[i],self.s,self.dt)
@@ -116,17 +116,17 @@ class Attractor(object):
             k3_z = self.fxn3(self.y[i],self.x[i],self.z[i]+k2_z*(.5*self.dt),self.b,self.dt)
             k4_z = self.fxn3(self.y[i],self.x[i],self.z[i]+k3_z*self.dt,self.b,self.dt)
             self.z[i+1] = self.z[i] + (1.0/6.0)*(k1_z + 2*k2_z + 2*k3_z + k4_z)
-        
+
             self.t[i+1] = self.t[i] + self.dt
-        
+
         return (self.x, self.y, self.z)
-    
-    def evolve(self,r0=[.1,0.0,0.0], order=4):
+
+    def evolve(self,r0=[1.0,0.0,0.0], order=4):
         """Define evolve.  Create numpy array r0 with default values.  Allow selection of euler, RK2, or RK4."""
         x0 = r0[0]
         y0 = r0[1]
         z0 = r0[2]
-        
+
         if order == 1:
             self.euler(r0)
         elif order == 2:
@@ -135,39 +135,39 @@ class Attractor(object):
             self.rk4(r0)
 
         self.solution = pd.DataFrame({'time': self.t, 'x': self.x, 'y': self.y, 'z': self.z})
-        
+
         return self.solution
-        
+
     def save(self):
         """Define save to csv"""
         self.solution.to_csv('data.csv')
               
     def plotx(self):
-        """Plot x"""
+        """Define plot x"""
         self.solution.plot(x='time',y='x',style='g-', title = 'x(t) vs Time Solution')
     
     def ploty(self):
-        """Plot y"""
+        """Define plot y"""
         self.solution.plot(x='time',y='y',style='b-', title = 'y(t) vs Time Solution')
     
     def plotz(self):
-        """Plot z"""
+        """Define plot z"""
         self.solution.plot(x='time',y='z',style='r-', title = 'z(t) vs Time Solution')
     
     def plotxy(self):
-        """Plot xy"""
+        """Define plot xy"""
         self.solution.plot(x='x',y='y',style='y-', title = 'x(t) vs Time Solution')
     
     def plotyz(self):
-        """Plot yz"""
+        """Define plot yz"""
         self.solution.plot(x='y',y='z',style='m-', title = 'y(t) vs Time Solution')
     
     def plotzx(self):
-        """Plot zx"""
+        """Define plot zx"""
         self.solution.plot(x='z',y='x',style='c-', title = 'z(t) vs Time Solution')
     
     def plot3d(self):
-        """3D plot"""
+        """Define 3d plot"""
         fig = plt.figure()
         ax = fig.gca(projection='3d')
         ax.plot_trisurf(self.x, self.y, self.z, cmap=cm.jet, linewidth=.2)
@@ -177,13 +177,11 @@ class Attractor(object):
         plt.show()         
         
     def plot3dwf(self):
-        """3D wireframe plot"""
+        """Define 3d wireframe plot"""
         fig = plt.figure()
         ax = fig.gca(projection='3d')
-        ax.plot_wireframe(self.x, self.y, self.z, rstride=10, cstride=10)
+        ax.plot_wireframe(self.x, self.y, self.z, rstride=1, cstride=1)
         ax.set_xlabel('x(t)')
         ax.set_ylabel('y(t)')
         ax.set_zlabel('z(t)')
-        plt.show()         
-            
-            
+        plt.show()
--- test_attractor.py
+++ test_attractor.py
@@ -13,7 +13,7 @@ from random import randint
 #import unittest
 
 class attr_test:
-    """Test setup and default/overide initial parameters"""
+    """Test setup and default/overide initial parameters.  This test evaluates if the initial parameters can be modified to accept something other than the default values.  """
     def setup(self):
         s = randint(1,10)
         p = randint(1,10)
@@ -23,30 +23,30 @@ class attr_test:
         attr.p_t = p
         attr.b_t = b
         attr = Attractor(attr.s_t, attr.p_t, attr.b_t)
-        assert attr.params[0] == s
-        assert attr.params[1] == p
-        assert attr.params[2] == b
-        assert attr.start == 0
-        assert attr.end == 80
-        assert attr.points == 10000
+        assert attr.params[0] == s, "\nError in assignment of value for s parameter"
+        assert attr.params[1] == p, "\nError in assignment of value for p parameter"
+        assert attr.params[2] == b, "\nError in assignment of value for b parameter"
+        assert attr.start == 0, "\nError in initial value for start parameter"
+        assert attr.end == 80, "\nError in initial value for end parameter"
+        assert attr.points == 10000, "\nError in initial value for points parameter"
 
     def test_euler(self):
         attr = Attractor()
         attr.evolve([10,10,10],1)
-        assert attr.solution['x'].count() > 0
-        assert attr.solution['y'].count() > 0
-        assert attr.solution['z'].count() > 0
+        assert attr.solution['x'].count() > 0, "\nError in solution for x using Euler method, Order=1"
+        assert attr.solution['y'].count() > 0, "\nError in solution for y using Euler method, Order=1"
+        assert attr.solution['z'].count() > 0, "\nError in solution for z using Euler method, Order=1"
 
     def test_rk2(self):
         attr = Attractor()
         attr.evolve([10,10,10],2)
-        assert attr.solution['x'].count() > 0
-        assert attr.solution['y'].count() > 0
-        assert attr.solution['z'].count() > 0
+        assert attr.solution['x'].count() > 0, "\nError in solution for x using rk2 method, Order=2"
+        assert attr.solution['y'].count() > 0, "\nError in solution for y using rk2 method, Order=2"
+        assert attr.solution['z'].count() > 0, "\nError in solution for z using rk2 method, Order=2"
 
     def test_rk4(self):
         attr = Attractor()
         attr.evolve([10,10,10],4)
-        assert attr.solution['x'].count() > 0
-        assert attr.solution['y'].count() > 0
-        assert attr.solution['z'].count() > 0
+        assert attr.solution['x'].count() > 0, "\nError in solution for x using rk4 method, Order=4"
+        assert attr.solution['y'].count() > 0, "\nError in solution for y using rk4 method, Order=4"
+        assert attr.solution['z'].count() > 0, "\nError in solution for z using rk4 method, Order=4"
